9a98edd6040d38bf5e7313503bed03041babe913
KazuBurrows <kazuiscool@gmail.com>
2025-06-15 12:26:46 +1200

    fixed commit issues

--- src/componets/CreateRecord.tsx
+++ src/componets/CreateRecord.tsx
@@ -114,23 +114,23 @@ export const groupedOptions: readonly GroupedOption[] = [
   },
 ];
 
-const groupStyles = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "space-between",
-};
-const groupBadgeStyles: CSSProperties = {
-  backgroundColor: "#EBECF0",
-  borderRadius: "2em",
-  color: "#172B4D",
-  display: "inline-block",
-  fontSize: 12,
-  fontWeight: "normal",
-  lineHeight: "1",
-  minWidth: 1,
-  padding: "0.16666666666667em 0.5em",
-  textAlign: "center",
-};
+// const groupStyles = {
+//   display: "flex",
+//   alignItems: "center",
+//   justifyContent: "space-between",
+// };
+// const groupBadgeStyles: CSSProperties = {
+//   backgroundColor: "#EBECF0",
+//   borderRadius: "2em",
+//   color: "#172B4D",
+//   display: "inline-block",
+//   fontSize: 12,
+//   fontWeight: "normal",
+//   lineHeight: "1",
+//   minWidth: 1,
+//   padding: "0.16666666666667em 0.5em",
+//   textAlign: "center",
+// };
 
 // const formatGroupLabel = (data: GroupedOption) => (
 //   <div style={groupStyles}>
--- src/views/Log.tsx
+++ src/views/Log.tsx
@@ -188,7 +188,7 @@ export default function Log() {
 
     // fetchRecords();
     checkCookieExpiration();
-  }, []);
+  }, [checkCookieExpiration,navigate, token]);
 
   const RenderThis = () => {
     if (isRetrievingData) {
