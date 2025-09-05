57a80646486dd8d1f0f2f09bfe343184f5810273
KazuBurrows <kazuiscool@gmail.com>
2025-09-06 01:51:54 +1200

    changed api call name

--- src/views/Log.tsx
+++ src/views/Log.tsx
@@ -46,10 +46,10 @@ export default function Log() {
       try {
         
         fetch(
-          "https://logmate.azurewebsites.net/api/ActivateOneLifeUrl?token=" +
+          "https://logmate.azurewebsites.net/api/GetLogData?token=" +
             token +
             ""
-          // "http://localhost:7071/api/ActivateOneLifeUrl?token=" + token + ""
+          // "http://localhost:7071/api/GetLogData?token=" + token + ""
         )
         .then((response) => {
           if (!response.ok) {
