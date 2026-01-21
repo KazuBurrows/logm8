56c3af6994bd7138c8ccd22e77059ee85d33a723
KazuBurrows <kazuki.burrows@gmail.com>
2026-01-21 18:40:23 +1300

    2 stroke ratio options added

--- src/types/serviceOptions.ts
+++ src/types/serviceOptions.ts
@@ -90,6 +90,25 @@ export const petrolOptions: readonly TaskOption[] = [
   { value: "100 Octane", label: "100 Octane" },
 ];
 
+export const ratioOptions: readonly TaskOption[] = [
+  { value: "Ratio 100:1", label: "Ratio 100:1" },
+  { value: "Ratio 90:1", label: "Ratio 90:1" },
+  { value: "Ratio 80:1", label: "Ratio 80:1" },
+  { value: "Ratio 75:1", label: "Ratio 75:1" },
+  { value: "Ratio 60:1", label: "Ratio 60:1" },
+  { value: "Ratio 55:1", label: "Ratio 55:1" },
+  { value: "Ratio 50:1", label: "Ratio 50:1" },
+  { value: "Ratio 45:1", label: "Ratio 45:1" },
+  { value: "Ratio 40:1", label: "Ratio 40:1" },
+  { value: "Ratio 35:1", label: "Ratio 35:1" },
+  { value: "Ratio 32:1", label: "Ratio 32:1" },
+  { value: "Ratio 30:1", label: "Ratio 30:1" },
+  { value: "Ratio 25:1", label: "Ratio 25:1" },
+  { value: "Ratio 20:1", label: "Ratio 20:1" },
+  { value: "Ratio 16:1", label: "Ratio 16:1" },
+];
+
+
 export const dieselOptions: readonly TaskOption[] = [
   { value: "Standard Diesel", label: "Standard Diesel" },
 ];
@@ -104,6 +123,10 @@ export const groupedFuelOptions: readonly GroupedOption[] = [
   {
     label: "Petrol (Gasoline)",
     options: petrolOptions,
+  },
+  {
+    label: "2 Stoke Ratios",
+    options: ratioOptions,
   },
   {
     label: "Diesel",
